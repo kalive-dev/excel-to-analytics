52a1c88abfb7430094285b080146654dee260ed5
Саша Пасека <sashapaseka2006@gmail.com>
2024-06-01 11:48:57 +0300

    updated class which include Machine Learning

--- excel-to-analytics.Client/ExcelAnalyzer.cs
+++ excel-to-analytics.Client/ExcelAnalyzer.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OfficeOpenXml;
-using MathNet.Numerics.Statistics;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.TimeSeries;
 
 namespace excel_to_analytics.Client
 {
     public class ExcelAnalyzer
     {
-        public static Dictionary<string, List<(DateTime Date, double Sales)>> LoadData(string filePath)
+        static Dictionary<string, List<SalesData>> LoadData(string filePath)
         {
-            var data = new Dictionary<string, List<(DateTime Date, double Sales)>>();
+            var data = new Dictionary<string, List<SalesData>>();
 
             using (var package = new ExcelPackage(new System.IO.FileInfo(filePath)))
             {
@@ -22,17 +23,38 @@ namespace excel_to_analytics.Client
                 {
                     var product = worksheet.Cells[row, 1].Text;
                     var date = DateTime.Parse(worksheet.Cells[row, 2].Text);
-                    var sales = double.Parse(worksheet.Cells[row, 3].Text);
+                    var sales = float.Parse(worksheet.Cells[row, 3].Text);
 
                     if (!data.ContainsKey(product))
                     {
-                        data[product] = new List<(DateTime Date, double Sales)>();
+                        data[product] = new List<SalesData>();
                     }
-                    data[product].Add((date, sales));
+                    data[product].Add(new SalesData { Product = product, Date = date, Sales = sales });
                 }
             }
 
             return data;
+        }
+
+        static List<float> ForecastSales(MLContext mlContext, List<SalesData> salesData)
+        {
+            var dataView = mlContext.Data.LoadFromEnumerable(salesData);
+
+            var forecastingPipeline = mlContext.Forecasting.ForecastBySsa(
+                outputColumnName: nameof(SalesPrediction.ForecastedSales),
+                inputColumnName: nameof(SalesData.Sales),
+                windowSize: 12,
+                seriesLength: salesData.Count,
+                trainSize: salesData.Count,
+                horizon: 12);
+
+            var model = forecastingPipeline.Fit(dataView);
+
+            var forecastingEngine = model.CreateTimeSeriesEngine<SalesData, SalesPrediction>(mlContext);
+
+            var forecast = forecastingEngine.Predict();
+
+            return forecast.ForecastedSales;
         }
         public static (DateTime MaxItem, double MaxValue) AnalyzeSeasonality(List<(DateTime Date, double Sales)> salesData)
         {
@@ -47,25 +69,23 @@ namespace excel_to_analytics.Client
 
             return (seasonalPeriods.Date, seasonalPeriods.Deviation);
         }
-
-        static void GenerateReport(Dictionary<string, string> classification)
+        static string ClassifySeasonality(List<float> forecastedSales)
         {
-            using (var package = new ExcelPackage())
+            // Аналіз піків продажів для визначення сезонності
+            var maxSales = forecastedSales.Max();
+            var maxIndex = forecastedSales.IndexOf(maxSales);
+
+            if (maxIndex == 11) // Припустимо, що 11 місяць - грудень
             {
-                var worksheet = package.Workbook.Worksheets.Add("Seasonal Classification");
-
-                worksheet.Cells[1, 1].Value = "Product";
-                worksheet.Cells[1, 2].Value = "Seasonality";
-
-                int row = 2;
-                foreach (var item in classification)
-                {
-                    worksheet.Cells[row, 1].Value = item.Key;
-                    worksheet.Cells[row, 2].Value = item.Value;
-                    row++;
-                }
-
-                package.SaveAs(new System.IO.FileInfo("seasonal_classification_report.xlsx"));
+                return "Christmas";
+            }
+            else if (maxIndex == 6) // Припустимо, що 6 місяць - липень
+            {
+                return "Summer";
+            }
+            else
+            {
+                return "No clear seasonality";
             }
         }
     }
